a3c8d5407be105d73f42472621baa869fe96e175
dede zakaria <105960257+zakarial0@users.noreply.github.com>
2024-07-03 12:49:23 +0000

    test bentrok kaga

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const KontakPage = () => {
+    return(
+        <Layout>
+            <h1>test nambah kata-kata buat di kontak</h1>
+        </Layout>
+    )
+}
+
+export default KontakPage;
