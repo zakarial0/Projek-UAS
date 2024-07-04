227b1909798b5403fda3e7dd6172eefed0b62ac3
Hazama32 <bintanghack32@gmail.com>
2024-07-04 11:10:46 +0000

    about.js

---
+++ src/pages/about.js
@@ -0,0 +1,34 @@
+import React from "react"
+<html>
+    <title>About Us</title>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section class="about">
+            <h1>About Us</h1>
+            <p>Total clean mart adalah sebuah platform website yang menyediakan berbagai macam barang kebersihan untuk meringankan pekerjaan rumah anda</p>
+            <h2>Misi Kami</h2>
+            <p>"Kebersihan bukanlah tujuan, tapi merupakan gaya hidup untuk menciptakan lingkungan yang sehat dan nyaman bagi semua."</p>
+            <h2>Tim Kami</h2>
+            <ul>
+                <li>
+                    <h3>John Doe</h3>
+                    <p>Founder & CEO</p>
+                </li>
+                <li>
+                    <h3>Jane Doe</h3>
+                    <p>Co-Founder & CTO</p>
+                </li>
+            </ul>
+        </section>
+    </main>
+    </body>
+</html>
